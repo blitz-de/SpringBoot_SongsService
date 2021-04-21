bfcc2e7879126ecb87adcbe307d3da0fa146bc12
finkin <davis.frank@satis-information.de>
2021-04-21 20:21:24 +0200

    analyse grouping added, methods added, static main methods added

--- runmerunner/src/main/java/htwb/ai/MainTestMethods.java
+++ runmerunner/src/main/java/htwb/ai/MainTestMethods.java
@@ -3,13 +3,13 @@ package htwb.ai;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 public class MainTestMethods {
 	
 	public static void main (String[] args) {
-		
-		int success= 0, failed=0, total=0, disabled = 0;
+
 		
 //		Scanner input = new Scanner(System.in);
 //		System.out.println("Enter Class Name: " );
@@ -21,116 +21,93 @@ public class MainTestMethods {
 //		if (args.isEmpty()) { //classInput -was
 //			System.out.println("no input");
 //		}
-//		
+//
 		if (args.length ==0 ) {
 			System.out.println("no-input");
 			System.out.println("Proper Usage is: java program filename");
 			System.exit(0);
 		}
+
+		for(int i = 0; i<args.length;i++){
+			searchForClass(args[i]);
+		}
 		
-		String className = args[0];
-		System.out.println("Entered Class is: " +className);
-		
-		if (args.length >0) { //!classInput.isEmpty -was
+
+	}
+	static void searchForClass(String className){
+		System.out.println("> search class:"+ className);
+		System.out.println("---------------");
+		int success= 0, failed=0, total=0, disabled = 0;
+		ArrayList<Method> withoutRunMeMethods = new ArrayList<>();
+		ArrayList<Method> runMeMethods = new ArrayList<>();
+		ArrayList<Method> notInvokeableMethods = new ArrayList<>();
 			try {
-				System.out.println("ClassName: " +className);
 				Class<?> clazz = Class.forName(className);
-//				
-//				
-//				System.out.println("getMethods: ");
-//				Method[] methods = clazz.getMethods();
-	//			Arrays.stream(methods).forEach(x -> System.out.println(x.getName()));
-				
-				System.out.println("getDeclaredMethods: ");
+
+				//System.out.println("getDeclaredMethods: ");
 				Method[] declMethods = clazz.getDeclaredMethods();
-				
+				System.out.println("trying to invoke methods with @runMe...");
 				for (Method m: declMethods) {
-					
+
 					// RUNME
-					
 					if (m.isAnnotationPresent(RunMe.class)) {
 						Annotation annotation = m.getAnnotation(RunMe.class);
 						RunMe runMeMethod = (RunMe) annotation;
-						System.out.println("Methods with @runMe");
 						if (runMeMethod.enabled()) {
-							
+
 							String result = "n/a";
-							
 							try {
 								result = (String) m.invoke(clazz.newInstance());
-								System.out.printf("%s - runMeMethod '%s' - processed %n - result: %n",
+								/*System.out.printf("%s - runMeMethod '%s' - processed %n - result: %n",
 										++total,
 										m.getName(),
-										result);
+										result);*/
+								runMeMethods.add(m);
+								++total;
 								success++;
 							} catch (Throwable ex) {
-								System.out.printf("%s - non-invokable '%s' - didn't process: %s %n",
+								/*System.out.printf("%s - non-invokable '%s' - didn't process: %s %n",
 										++total,
 										m.getName(),
-										ex.getCause());
-								
+										ex.getCause());*/
+								notInvokeableMethods.add(m);
+								++total;
 								failed++;
 							}
-						} //if - child1
-						
-						else {
-							System.out.println("Methods without @runMe");
-							
-							System.out.printf("%s - NoRunMe '%s' - didn't process%n",
-									++total,
-									m.getName()
-									);
-							disabled++;
-						} //else2
-					} //if - parent - RUNME-METHODS
-					//--------------------------------------------
-					// start -second if- NoRUNME-Methods
-					
-//					if (!m.isAnnotationPresent(RunMe.class)) {
-//						//Annotation annotation = m.getAnnotation(RunMe.class);
-//						//RunMe runMeMethod = (RunMe) annotation;
-//						//if (!runMeMethod.enabled()) {
-//							
-//							String result = "n/a";
-//							
-//							try {
-//								result = (String) m.invoke(clazz.newInstance());
-//								System.out.printf("%s - NoRunMeMethod '%s' - processed %n - result: %n", 
-//										++total,
-//										m.getName(),
-//										result);
-//								success++;
-//							} catch (Throwable ex) {
-//								System.out.printf("%s - NoRunMeMethod '%s' - didn't process: %s %n",
-//										++total,
-//										m.getName(),
-//										ex.getCause());
-//								failed++;
-//							}
-//						//} //if - child1
-//						//else {
-//							System.out.printf("%s - NoRunMeMethod '%s' - didn't process%n",
-//									++total,
-//									m.getName());
-//							disabled++;
-//						//} //else2
-					//} 
-					//End of - second if - NoRunME-Methods
-					//-----------------------------------------
-				//	System.out.println(m.getName());
-				} //for
-				
+						}
+					} else {
+						withoutRunMeMethods.add(m);
+						++total;
+						disabled++;
+					}
+				}
+				System.out.println("###############");
+				System.out.println("printing method groups...");
+				printMethods(withoutRunMeMethods, "Methods without @RunMe:");
+				printMethods(runMeMethods, "Methods with @RunMe:");
+				printMethods(notInvokeableMethods, "not invocable:");
+
 				System.out.printf("%nResult: Total : %d, Successful: %d, failed %d, Disabled %d%n",
 						total,
 						success,
 						failed,
 						disabled);
-				
+
+
 			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
+				System.out.println("Error: Could not find class "+className);
+				System.out.println("Usage: java -jar runmerunner-TEAMNAME.jar className");
 			}
-		}
 	}
+	static void printMethods(ArrayList<Method> methods, String message){
+		System.out.println("---------------");
+		System.out.println(message);
+		for (Method m: methods) {
+			System.out.println(m.getName());
+		}
+
+	}
+
 	
 	static Object loadClass(Class<?> clazz) throws InstantiationException,
 	IllegalAccessException,
