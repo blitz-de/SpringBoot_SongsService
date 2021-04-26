cbc2226c3b3041c5c1fd24735cf1f42ceffd70a3
Thund-de <s0562218@htw-berlin.de>
2021-04-26 09:47:52 +0200

    added examples to usage message and edited readme

--- runmerunner/src/main/java/htwb/ai/app/App.java
+++ runmerunner/src/main/java/htwb/ai/app/App.java
@@ -14,13 +14,14 @@ public class App {
     public static void main(String[] args) throws NoInputException {
     	
     	AnnoVsNoAnnoMethods allMethods = new AnnoVsNoAnnoMethods();
-        if (args.length == 0) {
+
+    	if (args.length == 0) {
 
             NoInputException ex = new NoInputException("No Parameter [className] was given to main method!");
             System.out.println("Error: " +
                     ex.getClass().getSimpleName());
-            System.out.println("Usage: java -jar path/to/jar/runmerunner.jar className" +
-                    ex.getClass().getSimpleName());
+            System.out.println("Usage: java -jar path/to/jar/runmerunner.jar Packagepath.className" +"\n"
+                    +"\nfor example: java -jar runmerunner-sakvis.jar htwb.ai.RunMeMethods");
             //Um die Main-Methode testbar zu machen werfen wird eine NoInputException
             throw new NoInputException(ex.getClass().getSimpleName());
 
