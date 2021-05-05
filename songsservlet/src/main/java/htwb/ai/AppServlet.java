3ec9069d1ea3acf64379aca18d17076bbc96d2a8
finkin <davis.frank@satis-information.de>
2021-05-05 14:57:57 +0200

    servlet configuration

---
+++ songsservlet/src/main/java/htwb/ai/AppServlet.java
@@ -0,0 +1,74 @@
+package htwb.ai;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+@WebServlet("/songs")
+public class AppServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private String uriToDB = null;
+
+    @Override
+    public void init(ServletConfig servletConfig)
+            throws ServletException {
+        // Beispiel: Laden eines Initparameters
+        // aus der web.xml
+        this.uriToDB = servletConfig
+                .getInitParameter("uriToDB");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+        // alle Parameternamen (keys)
+        Enumeration<String> paramNames = request
+                .getParameterNames();
+
+        String responseStr = "";
+        String param = "";
+        while (paramNames.hasMoreElements()) {
+            param = paramNames.nextElement();
+            responseStr = responseStr + param + "="
+                    + request.getParameter(param) + "\n";
+        }
+
+        System.out.println ("ECHO-SERVLET: " + responseStr );
+        response.setContentType("text/plain");
+        try (PrintWriter out = response.getWriter()) {
+            responseStr += "\n Your request will be sent to "
+                    + uriToDB;
+            out.println(responseStr);
+        }
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+        ServletInputStream inputStream = request
+                .getInputStream();
+        byte[] inBytes = IOUtils.toByteArray(inputStream);
+        try (PrintWriter out = response.getWriter()) {
+            out.println(new String(inBytes));
+        }
+    }
+
+    protected String getUriToDB () {
+        return this.uriToDB;
+    }
+}
---
+++ songsservlet/src/test/java/htwb/ai/AppServletTest.java
@@ -0,0 +1,63 @@
+package htwb.ai;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletConfig;
+
+public class AppServletTest {
+
+    private AppServlet servlet;
+    private MockServletConfig config;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    private final static String URITODB_STRING = "uriToDB:test.server";
+
+    @BeforeEach
+    public void setUp() throws ServletException {
+        servlet = new AppServlet();
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        config = new MockServletConfig();
+        config.addInitParameter("uriToDB", URITODB_STRING);
+        servlet.init(config); // throws ServletException
+    }
+
+    @Test
+    public void initShouldSetDBComponentURI() {
+        assertEquals(URITODB_STRING, servlet.getUriToDB());
+    }
+
+    @Test
+    public void doGetShouldEchoParameters() throws ServletException, IOException {
+        request.addParameter("username", "scott");
+        request.addParameter("password", "tiger");
+
+        servlet.doGet(request, response);
+
+        assertEquals("text/plain", response.getContentType());
+        assertTrue(response.getContentAsString().contains("username=scott"));
+        assertTrue(response.getContentAsString().contains("password=tiger"));
+        assertTrue(response.getContentAsString().contains(URITODB_STRING));
+    }
+
+    @Test
+    public void doPostShouldEchoBody() throws ServletException, IOException {
+        request.setContent("blablablabla".getBytes());
+        servlet.doPost(request, response);
+        assertEquals("text/plain", response.getContentType());
+        assertTrue(response.getContentAsString().contains("blablablabla"));
+    }
+}
