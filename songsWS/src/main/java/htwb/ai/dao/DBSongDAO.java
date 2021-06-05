a1ee62510448014321ada353b2857a76230a8ead
Thund-de <s0562218@htw-berlin.de>
2021-06-05 15:47:46 +0200

    added SongDAO

---
+++ songsWS/src/main/java/htwb/ai/dao/DBSongDAO.java
@@ -0,0 +1,45 @@
+package htwb.ai.dao;
+
+import java.util.List;
+
+import htwb.ai.model.User;
+
+public class DBSongDAO implements ISongDAO {
+
+	@Override
+	public User getUserById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserByUserId(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer save(User User) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateUser(User User) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteUser(String userId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
